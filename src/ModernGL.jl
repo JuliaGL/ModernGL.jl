e1f6e62f0a1cc7c7166e3e853bd2c1e1e365a57a
A.A.Alexandrov <greyalex2003@yahoo.com>
2015-01-23 01:04:19 +0200

    Switched all GL functions to be generated by a getCFun style macro. Made a macro that implements a deferred ccall initialization function pointer initialization.

--- src/ModernGL.jl
+++ src/ModernGL.jl
@@ -32,7 +32,13 @@ function NSGetProcAddress(glFuncName::ASCIIString)
 end
 
 function wglGetProcAddress(glFuncName::ASCIIString)
-    ccall((:wglGetProcAddress, "opengl32"), Ptr{Void}, (Ptr{Uint8},), glFuncName)
+    p = ccall((:wglGetProcAddress, "opengl32"), Ptr{Void}, (Ptr{Uint8},), glFuncName)
+    if p == C_NULL
+        lib = dlopen_e("opengl32")
+        p = dlsym_e(lib, glFuncName)
+        dlclose(lib)
+    end
+    p
 end
 
 
@@ -49,7 +55,7 @@ function getprocaddress(glFuncName::ASCIIString)
 end
 
 # Test, if an opengl function is available.
-# Sadly, this doesn't work for Linux, as glxGetProcAddress 
+# Sadly, this doesn't work for Linux, as glxGetProcAddress
 # always returns a non null function pointer, as the function pointers are not depending on an active context.
 #
 
@@ -59,9 +65,9 @@ function isavailable(name::Symbol)
 end
 function isavailable(ptr::Ptr{Void})
     return !(
-        ptr == C_NULL || 
-        ptr == convert(Ptr{Void},  1) || 
-        ptr == convert(Ptr{Void},  2) || 
+        ptr == C_NULL ||
+        ptr == convert(Ptr{Void},  1) ||
+        ptr == convert(Ptr{Void},  2) ||
         ptr == convert(Ptr{Void},  3))
 end
 
@@ -78,6 +84,52 @@ macro getFuncPointer(func)
         end
         $z::Ptr{Void}
     end end
+end
+
+
+type GLFunc
+    p::Ptr{Void}
+end
+
+const __ofTypeSym = symbol("::")
+
+# based on getCFun macro
+macro glfunc(cFun)
+    arguments = map(function (arg)
+                        if isa(arg, Symbol)
+                            arg = Expr(__ofTypeSym, arg)
+                        end
+                        return arg
+                    end, cFun.args[1].args[2:end])
+
+    # Get info out of arguments of `cFun`
+    argumentNames = map(arg->arg.args[1], arguments)
+    returnType    = cFun.args[2]
+    inputTypes    = map(arg->arg.args[2], arguments)
+    fnName        = cFun.args[1].args[1]
+
+    varName       = symbol("$(fnName)_ptr")
+
+    ccallExpr = Expr(:ccall, :($varName.p), returnType, Expr(:tuple, inputTypes...), argumentNames...)
+    ccallFun  = Expr(:function, Expr(:call, fnName, argumentNames...), ccallExpr)
+    typedCcall = returnType == :Void? ccallExpr : :($ccallExpr::$returnType)
+
+    initName = symbol("init_$fnName")
+    ret = quote
+        const $varName = GLFunc(C_NULL)
+        function $initName($(argumentNames...))
+            $varName.p = getprocaddress($(string(fnName)))
+            if $varName.p == C_NULL
+                error($(string(fnName)), " not available for your driver, or no valid OpenGL context available")
+            end
+            $typedCcall
+        end
+        $varName.p = cfunction($initName, $returnType, $(Expr(:tuple, inputTypes...)))
+        $ccallFun
+        $(Expr(:export, fnName))
+    end
+
+    return esc(ret)
 end
 
 abstract Enum
@@ -99,7 +151,7 @@ macro GenEnums(list)
             name::Symbol
         end
         $(dictname) = $enumdict1
-        function $(enumName){T}(number::T) 
+        function $(enumName){T}(number::T)
             if !haskey($(dictname), number)
                 error("x is not a GLenum")
             end
